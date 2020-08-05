0481ba958a19d2a6e6df65220350889404fd5908
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-08-04 22:55:00 -0500

    added index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+const express = require("express"); // CommonJS module
+const server = express();
+const PORT = 8000; // we visit http://localhost:8000/ to see the api
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
