a6dd30e8e27a906c53c7e92648ff85a9d011b606
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-08-04 23:04:35 -0500

    MVP

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const express = require("express"); // CommonJS module
+const express = require("express");
+
+const postsRouter = require("./posts/posts-router.js");
+
 const server = express();
-const PORT = 8000; // we visit http://localhost:8000/ to see the api
-server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).send("<h1>The server is working</h1>");
+});
+
+server.use("/api/posts", postsRouter);
+
+const PORT = 8000;
+server.listen(PORT, () => {
+  console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
+});
